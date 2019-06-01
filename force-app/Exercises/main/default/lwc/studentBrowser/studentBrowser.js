23c11c10233cf4915f21710fe89ee825f158fe90
Your Name <you@example.com>
2019-06-01 10:18:45 -0700

    Exercise 2-7 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,25 +1,14 @@
 
-import { LightningElement, track } from "lwc";
+import { LightningElement, wire } from 'lwc';
+import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+
 
 
 export default class StudentBrowser extends LightningElement {
 
-	@track studentList = [];
+	//@track studentList = [];
+	@wire(getStudents, { instructorId: "", courseDeliveryId: ""}) students;
 
-	constructor() {
-		super();
-		const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul',
-			"Amit", "Simon"];
-		studentNames.forEach((studentName, index) => {
-			this.studentList.push(
-				{
-					'sobjectType': 'Contact',
-					'Name': studentName,
-					'PhotoUrl': '/services/images/photo/003B0FakePictId',
-					'Id': index
-				}
-			);
-		})
-	}
+	
 
 }
